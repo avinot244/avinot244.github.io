4abc29e08ccf850d209e0aad52a90ea6f6c9e577
Aymeric <avinot@enssat.fr>
2022-02-25 09:41:39 +0100

    v

--- js/scriptTP4exo1.js
+++ js/scriptTP4exo1.js
@@ -2,7 +2,7 @@ function main() {
     alert("Hello World");
     if (window.Worker){
         console.log("Création du worker");
-        var monWorker = new Worker("../js/worker/workerTP4exo1.js");
+        var monWorker = new Worker("workerTP4exo1.js");
         console.log("coucou");
         monWorker.onmessage = function(e){
             console.log("Message reçu avec nb = "+e.data[0]);
