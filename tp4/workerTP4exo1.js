b7e3aedaf918f808d71124f145a9055ea275a665
Aymeric <avinot@enssat.fr>
2022-02-25 09:52:00 +0100

    z

--- tp4/workerTP4exo1.js
+++ tp4/workerTP4exo1.js
@@ -1,7 +1,8 @@
 let intervalle = setInterval(workerLoop,1000);
 function workerLoop() {
     console.log("Création message du worker");
-    var nb = Math.floor(Math.random() * 10);
+    var x = Math.floor(Math.random() * 10);
+    var y = Math.floor(Math.random() * 10);
     console.log("envoi du message au script principal");
-    this.postMessage([nb]);
-}//
+    this.postMessage([x, y]);
+}
